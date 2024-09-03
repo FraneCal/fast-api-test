0a692f7b893b623ee207cc373b690ea4f33b199b
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-09-03 21:45:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return {"message": "hello world"}
+class Numbers(BaseModel):
+    numbers: List[int]
+
+@app.post("/process/")
+async def process_numbers(data: Numbers):
+    result = sum(data.numbers)
+    return {"result": result}
