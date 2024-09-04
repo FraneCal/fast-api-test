2bbf346455521571cda91ea83f3223260b901df7
Frane Ćalušić <90317417+FraneCal@users.noreply.github.com>
2024-09-04 13:18:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import PlainTextResponse
 from pydantic import BaseModel
 from typing import List
-from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
 class ListRequest(BaseModel):
      numbers: List[int]
 
-
-@app.post("/")
-
+@app.post("/", response_class=PlainTextResponse)
 async def root(request: ListRequest):
-    # int32 range
+     int32_min = -2**31
+     int32_max = 2**31 - 1
 
-    int32_min = -2**31
-
-    int32_max = 2**31 - 1
-
-    for number in request.numbers:
-         if not (int32_min <= number <= int32_max):
-             raise HTTPException(status_code=400, detail="All 
-            elements must be int32 numbers")
-
-
-    result = sum(request.numbers)
-
-    return f"Sum of numbers: {result}"` or `from fastapi.responses import PlainTextResponse
+     for number in request.numbers:
+          if not (int32_min <= number <= int32_max):
+               raise HTTPException(status_code=400,detail="All elements must be int32 numbers")
+     
+     result = sum(request.numbers)
+     
+     return f"Sum of numbers: {result}"
